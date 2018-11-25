ea4dd4b8d191d6bffed9b97f35b050c7348b43bb
Carl Smith <carlfsmithiii@gmail.com>
2018-11-25 17:51:30 -0500

    improved performance, passed short test

--- anagrams.py
+++ anagrams.py
@@ -20,7 +20,9 @@ def alphabetize(string):
         abc
 
     """
-    return "".join(sorted(string.lower()))
+    array = list(string)
+    array.sort()
+    return "".join(array)
 
 
 def find_anagrams(words):
@@ -35,20 +37,22 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        alpha_word = alphabetize(word)
+        try:
+            anagrams[alpha_word].append(word)
+        except KeyError:
+            anagrams[alpha_word] = [word]
     return anagrams
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            print(find_anagrams(words))
